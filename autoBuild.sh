c5e247990ea996009af4d076281fc7fdef15eaf3
jiangtaotao <torrent_jiang@163.com>
2023-03-16 14:28:29 +0800

    feat: 更新脚本文件

--- autoBuild.sh
+++ autoBuild.sh
@@ -1,5 +1,5 @@
 git pull origin master      # 拉取最新代码
 npm install                 # 安装依赖
 npm run docs:build          # 打包
-cd /usr/local/sbin/         # 重启nginx
-./nginx -s reload
+# cd /usr/local/sbin/         # 重启nginx
+# ./nginx -s reload
