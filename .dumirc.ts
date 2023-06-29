0712104d77ae5269b5694430962ede233eaa60fe
jiangtaotao <torrent_jiang@163.com>
2023-06-29 15:31:36 +0800

    fix: 修改图片资源路径

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  favicons: ['/favicon.ico'],
+  favicons: ['/home/favicon.ico'],
   autoAlias: false,
   locales: [{ id: 'zh-CN', name: '中文' }],
   outputPath: 'docs-dist',
@@ -9,7 +9,7 @@ export default defineConfig({
   base: '/home',
   sitemap: { hostname: 'http://www.torrentjiang.store' },
   themeConfig: {
-    logo: '/logo.png',
+    logo: '/home/logo.png',
     name: 'torrent',
     footer: '我沪ICP备2022034341号',
     socialLinks: {
