e161ae1caf9daea6433f60ceeedc287c80f36e46
jiangtaotao <torrent_jiang@163.com>
2023-09-15 18:46:38 +0800

    fix: 更新打包路径

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  favicons: ['/profile/favicon.ico'],
+  favicons: ['/home/favicon.ico'],
   autoAlias: false,
   locales: [{ id: 'zh-CN', name: '中文' }],
   outputPath: 'docs-dist',
@@ -9,7 +9,7 @@ export default defineConfig({
   base: '/home',
   sitemap: { hostname: 'https://www.torrentjiang.store' },
   themeConfig: {
-    logo: '/profile/logo.png',
+    logo: '/home/logo.png',
     name: 'torrent',
     footer: '沪ICP备2022034341号',
     socialLinks: {
